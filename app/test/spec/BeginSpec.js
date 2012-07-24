b1c9325e8199f5dd877cfd418d61c3523b9a50e8
Oliver Newland <onewland@gmail.com>
2012-07-24 12:07:39 -0700

    Add one reasonable test for Begin class
    Also add include in main.html

--- app/test/spec/BeginSpec.js
+++ app/test/spec/BeginSpec.js
@@ -1,9 +1,10 @@
 describe("Begin", function() {
+    var testNodeId = 2;
     var testNode;
     var testBegin;
 
     beforeEach(function() {
-	testNode = new window.sirius.Node({id: 2});
+	testNode = new window.sirius.Node({id: testNodeId});
 	testBegin = new window.sirius.Begin({node: testNode});
     });
 
@@ -16,4 +17,11 @@ describe("Begin", function() {
     it("should contain test node", function() {
 	expect(testBegin.get('node')).toEqual(testNode);
     });
+
+    it("should encode node reference as node_id", function() {
+	// should not contain node_id at first
+	expect(testBegin.get('node_id')).toBeUndefined();
+	testBegin.encode_references();
+	expect(testBegin.get('node_id')).toEqual(testNodeId);
+    })
 });
