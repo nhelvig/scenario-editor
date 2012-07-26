c27e258a3d964809d5f514e633b29974fa918f1a
Oliver Newland <onewland@gmail.com>
2012-07-26 15:01:15 -0700

    Added tests for Density

---
+++ app/test/spec/DensitySpec.js
@@ -0,0 +1,39 @@
+describe("Density", function() {
+    var testLinkId = 2;
+    var testLink;
+    var testDensity;
+
+    beforeEach(function() {
+	testLink = new window.sirius.Link({id: testLinkId});
+	testDensity = new window.sirius.Density({link: testLink});
+    });
+
+    it("should not blow up on to_xml", function() {
+	var doc = document.implementation.createDocument("document:xml", "begin", null);
+	var out = testDensity.to_xml(doc); 
+	expect(out).not.toBeNull();
+    });
+
+    it("should contain test link", function() {
+	expect(testDensity.get('link')).toEqual(testLink);
+    });
+
+    it("should resolve link_id as link", function() {
+	var b = new window.sirius.Density({link_id: testLinkId});
+	var deferred = [];
+	var object_with_id = { 'link': [] };
+	object_with_id.link[testLinkId] = testLink;
+	expect(b.get('link')).toBeUndefined();
+	b.resolve_references(deferred, object_with_id);
+	runDeferred(deferred);
+	expect(b.get('link')).toEqual(testLink);
+    });
+
+    it("should encode link reference as link_id", function() {
+	// should not contain link_id at first
+	expect(testDensity.get('link_id')).toBeUndefined();
+	testDensity.encode_references();
+	expect(testDensity.get('link_id')).toEqual(testLinkId);
+    })
+
+});
