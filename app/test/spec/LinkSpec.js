5c6b355c5ed1dc7dc3757a395171cc8851f3c5f8
Oliver Newland <onewland@gmail.com>
2012-07-27 09:45:54 -0700

    Move simpleLink to shared test helpers

--- app/test/spec/LinkSpec.js
+++ app/test/spec/LinkSpec.js
@@ -8,15 +8,18 @@ describe("Link", function() {
     var testLink1;
     var testLink2;
 
-    var simpleLink = function(node1, node2) {
-	var begin = new window.sirius.Begin({node: node1});
-	var end = new window.sirius.End({node: node2});
-	var link = new window.sirius.Link({begin: begin, end: end});
-	var output = [new window.sirius.Output({link: link})];
-	var outputs = new window.sirius.Outputs({output: output});
-	node1.set('outputs',outputs);
-	return link;
-    }
+    // var simpleLink = function(node1, node2) {
+    // 	var begin = new window.sirius.Begin({node: node1});
+    // 	var end = new window.sirius.End({node: node2});
+    // 	var link = new window.sirius.Link({begin: begin, end: end});
+    // 	var output = [new window.sirius.Output({link: link})];
+    // 	var outputs = new window.sirius.Outputs({output: output});
+    // 	var input = [new window.sirius.Input({link: link})];
+    // 	var inputs = new window.sirius.Inputs({input: input});
+    // 	node1.set('outputs',outputs);
+    // 	node2.set('inputs', inputs);
+    // 	return link;
+    // }
 
     beforeEach(function() {
 	testNode = new window.sirius.Node({id: testNodeId});
--- app/test/spec/SpecHelper.js
+++ app/test/spec/SpecHelper.js
@@ -2,4 +2,17 @@ beforeEach(function() {
     runDeferred = function(fList) {
       _.each(fList, function(f) { f(); });
     };
+    
+    simpleLink = function(node1, node2) {
+	var begin = new window.sirius.Begin({node: node1});
+	var end = new window.sirius.End({node: node2});
+	var link = new window.sirius.Link({begin: begin, end: end});
+	var output = [new window.sirius.Output({link: link})];
+	var outputs = new window.sirius.Outputs({output: output});
+	var input = [new window.sirius.Input({link: link})];
+	var inputs = new window.sirius.Inputs({input: input});
+	node1.set('outputs',outputs);
+	node2.set('inputs', inputs);
+	return link;
+    }
 });
