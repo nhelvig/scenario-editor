1182bdba5b9efbcd0e46c845192f3bbe3824b718
Oliver Newland <onewland@gmail.com>
2012-07-27 10:03:51 -0700

    Remove commented simpleLink from LinkSpec.js

--- app/test/spec/LinkSpec.js
+++ app/test/spec/LinkSpec.js
@@ -8,19 +8,6 @@ describe("Link", function() {
     var testLink1;
     var testLink2;
 
-    // var simpleLink = function(node1, node2) {
-    // 	var begin = new window.sirius.Begin({node: node1});
-    // 	var end = new window.sirius.End({node: node2});
-    // 	var link = new window.sirius.Link({begin: begin, end: end});
-    // 	var output = [new window.sirius.Output({link: link})];
-    // 	var outputs = new window.sirius.Outputs({output: output});
-    // 	var input = [new window.sirius.Input({link: link})];
-    // 	var inputs = new window.sirius.Inputs({input: input});
-    // 	node1.set('outputs',outputs);
-    // 	node2.set('inputs', inputs);
-    // 	return link;
-    // }
-
     beforeEach(function() {
 	testNode = new window.sirius.Node({id: testNodeId});
 	testNode2 = new window.sirius.Node({id: testNodeId2});
