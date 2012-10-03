3d3020c09395d56fe9cb7da2eb37ba4425a86c41
mnjuhn <mnjuhn@gmail.com>
2012-10-02 18:59:47 -0700

    Adding basic Unit test based on J-unit

---
+++ src/test/java/edu/berkeley/path/scenario_editor/TestScenarioDownloadServlet.java
@@ -0,0 +1,38 @@
+package edu.berkeley.path.scenario_editor;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for ScenarioDownloadServlet.
+ */
+public class TestScenarioDownloadServlet 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public TestScenarioDownloadServlet( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( TestScenarioDownloadServlet.class );
+    }
+
+    /**
+     * Rigourous Test ;)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
