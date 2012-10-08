36245c1e30398ec55aea9489a48b43e78fa65ede
Sean Morris <sean.smorris@gmail.com>
2012-10-07 21:52:43 -0700

    testing back up after restructure

--- src/main/webapp/app/test/load-jasmine.js
+++ src/main/webapp/app/test/load-jasmine.js
@@ -7,19 +7,25 @@
         'SignalSpec', 'SplitratioProfileSpec'];
     
   load_sirius_classes = function(after) {
-    return head.js("../js/Sirius.js",'../js/sirius-classes-load.js', function() {
+    return head.js("../js/models/Sirius.js",'../js/sirius-classes-load.js', function() {
       var class_paths;
-      class_paths = _.map(window.sirius.sirius_classes_without_extensions, function(cname) {
-        return "../js/" + cname + ".js";
+      class_paths = _.map(window.sirius.models_without_extensions, function(cname) {
+        return "../js/models/" + cname + ".js";
       });
-      class_paths = class_paths.concat(_.flatten(_.map(window.sirius.sirius_map_view_classes, function(cname) {
-        return "../js/" + cname + ".js";
+      class_paths = class_paths.concat(_.flatten(_.map(window.sirius.models_util, function(cname) {
+        return "../js/models/util/" + cname + ".js";
       })));
-      class_paths = class_paths.concat(_.flatten(_.map(window.sirius.sirius_model_view_classes, function(cname) {
-        return "../js/" + cname + ".js";
+      class_paths = class_paths.concat(_.flatten(_.map(window.sirius.models_with_extensions, function(cname) {
+        return ["../js/models/" + cname + ".js", "../js/models/extensions/" + cname + ".js"];
       })));
-      class_paths = class_paths.concat(_.flatten(_.map(window.sirius.sirius_classes_with_extensions, function(cname) {
-        return ["../js/" + cname + ".js", "../js/extensions/" + cname + ".js"];
+      class_paths = class_paths.concat(_.flatten(_.map(window.sirius.map_view_classes, function(cname) {
+        return "../js/views/" + cname + ".js";
+      })));
+      class_paths = class_paths.concat(_.flatten(_.map(window.sirius.collection_classes, function(cname) {
+        return "../js/collections/" + cname + ".js";
+      })));
+      class_paths = class_paths.concat(_.flatten(_.map(window.sirius.util_classes, function(cname) {
+        return "../js/util/" + cname + ".js";
       })));
       class_paths.push(after);
       return head.js.apply(this, class_paths);
@@ -43,7 +49,7 @@
   };
   
   window.loadSirius = function(after) {
-    return head.js("../js/Sirius.js", '../js/menu-data.js', 'scenario-xml.js', function() {
+    return head.js("../js/models/Sirius.js", '../js/menu-data.js', 'scenario-xml.js', function() {
       return load_sirius_classes(function() {
           return load_jasmine_specs_templates(after);
         });
