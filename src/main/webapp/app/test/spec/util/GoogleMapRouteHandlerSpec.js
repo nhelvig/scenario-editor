440403ef0d2d5aa6b03d2912c0425ad9b2780060
Sean Morris <sean.smorris@gmail.com>
2013-05-21 23:08:27 -0700

    GoogleMapRouteHandlerSpec testing complete

--- src/main/webapp/app/test/spec/util/GoogleMapRouteHandlerSpec.js
+++ src/main/webapp/app/test/spec/util/GoogleMapRouteHandlerSpec.js
@@ -5,52 +5,130 @@ describe("GoogleMapRouteHandler", function() {
   
   beforeEach(function() {
     links = $a.models.links();
-    this.gmr = new $a.GoogleMapRouteHandler(links);
+    gmr = new $a.GoogleMapRouteHandler(links);
+    l = gmr.links[0];
   });
   
   describe("Instantiation", function() {
     it("should create directions service", function() {
-      expect(this.gmr.directionsService ).not.toBeNull();
+      expect(gmr.directionsService ).not.toBeNull();
     });
-    it("should set stop to false", function() {
-      expect(this.gmr.stop).toBeFalsy();
+    it("stop should be false", function() {
+      expect(gmr.stop).toBeFalsy();
     });
-    it("should be listening to stopDrawing", function() {
-      spyOn($a.GoogleMapRouteHandler.prototype, 'stopDrawing');
-      $a.broker.trigger('map:clear_map');
-      expect($a.GoogleMapRouteHandler.prototype.stopDrawing).toHaveBeenCalled();
+    it("map:clear_map should be listening to stopDrawing", function() {
+      afterEach(function(){
+        gmr.stop = false;
+      })
+      runs(function() {
+         flag = false;
+         $a.broker.trigger('map:clear_map');
+         setTimeout(function() {flag = true;}, 500);
+       });
+       waitsFor(function() {
+         return flag;
+       }, "should have called stopDrawing by now", 750);
+       runs(function() { 
+         expect(gmr.stop).toBeTruthy();
+       });
     });
     
   });
 
   describe("stopDrawing", function() {
+     afterEach(function(){
+       gmr.stop = false;
+     })
      it("should set stop to true", function() {     
-        this.gmr.stopDrawing();
-        expect(this.gmr.stop).toBeTruthy();
+        gmr.stopDrawing();
+        expect(gmr.stop).toBeTruthy();
      });
    });
   
   describe("setUpLink", function() {
      it('should set up link for directions request', function() {     
-       link = this.gmrh.links[0]
-       this.gmrh.setUpLink(link);
-       expect(link.request).not.toBeNull();
-       expect(link.request).not.toBeNull();
-
+       gmr.setUpLink(l);
+       expect(l.request).not.toBeNull(l);
+       expect(l.request.origin).toEqual($a.Util.getLatLng(l.begin_node()));
+       expect(l.request.destination).toEqual($a.Util.getLatLng(l.end_node()));
+       expect(l.request.travelMode).toEqual(google.maps.TravelMode.DRIVING);
      });
    });
-  //  
-  //  describe("_isOverQuery", function() {
-  //    beforeEach(function() {
-  //      gStatus = google.maps.DirectionsStatus;
-  //    });
-  //    it("should be true if status over query limit", function() {
-  //      flag = this.gmrh._isOverQuery(gStatus.OVER_QUERY_LIMIT);
-  //      expect(flag).toBeTruthy();
-  //    });
-  //    it("should be false if status not over query limit", function() {
-  //      flag = this.gmrh._isOverQuery(gStatus.OK);
-  //      expect(flag).not.toBeTruthy();
-  //    });
-  //  });
+   
+   describe("_isOverQuery", function() {
+     beforeEach(function() {
+       gStatus = google.maps.DirectionsStatus;
+     });
+     it("should be true if status over query limit", function() {
+       flag = gmr._isOverQuery(gStatus.OVER_QUERY_LIMIT);
+       expect(flag).toBeTruthy();
+     });
+     it("should be false if status not over query limit", function() {
+       flag = gmr._isOverQuery(gStatus.OK);
+       expect(flag).not.toBeTruthy();
+     });
+   });
+   
+   describe("geomDoesNotExist", function() {
+      beforeEach(function(){
+        geometry = l.geometry();
+        position = l.position();
+      });
+      afterEach(function(){
+        l.set_geometry(geometry);
+        l.set_position(position);
+      })
+     it('should test to see if geometry exists', function() {     
+       flag = gmr._geomDoesNotExist(l)
+       expect(flag).toBeFalsy();
+       
+       l.set_geometry(null);
+       l.set_position(null);
+       flag = gmr._geomDoesNotExist(l);
+       expect(flag).toBeTruthy();
+     });
+   });
+   
+   describe("directionsRequest and directionsRequestOneLink", function() {
+      beforeEach(function(){
+        geometry = l.geometry();
+        position = l.position();
+        l.set_geometry(null);
+        l.set_position(null);
+      });
+      afterEach(function(){
+        l.set_geometry(geometry);
+        l.set_position(position);
+      })
+     it('directionsRequest: should have link position set', function() {
+       runs(function() {
+         flag = false;
+         gmr._directionsRequest(l);
+         setTimeout(function() {flag = true;}, 500);
+       });
+       waitsFor(function() {
+         return flag;
+       }, "The request should be done", 750);
+       runs(function() { 
+         expect(l).not.toBeNull();
+         expect(l.position()).not.toBeNull();
+       });
+     });
+     
+     it('directionsRequestOneLink: should have link position set', function() {
+       runs(function() {
+         flag = false;
+         gmr._directionsRequestOneLink(l);
+         setTimeout(function() {flag = true;}, 500);
+       });
+       waitsFor(function() {
+         return flag;
+       }, "The request should be done", 750);
+       runs(function() { 
+         expect(l).not.toBeNull();
+         expect(l.position()).not.toBeNull();
+       });
+     });
+   });
+   
 });
